392281ed97cf6915b288866b1f05572d7fbec99c
Lars Göttgens <lars.goettgens@rwth-aachen.de>
2025-12-02 11:19:30 +0100

    FLINT_jll build 301.400.1+0

---
+++ src/FLINT_jll.jl
@@ -0,0 +1,9 @@
+# Use baremodule to shave off a few KB from the serialized `.ji` file
+baremodule FLINT_jll
+using Base
+using Base: UUID
+import JLLWrappers
+
+JLLWrappers.@generate_main_file_header("FLINT")
+JLLWrappers.@generate_main_file("FLINT", Base.UUID("e134572f-a0d5-539d-bddf-3cad8db41a82"))
+end  # module FLINT_jll
